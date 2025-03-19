a8f9313a3da8971378edac738a33622bae30ba5a
Sahil-tarento <140611066+Sahil-tarento@users.noreply.github.com>
2025-03-19 14:13:54 +0530

    Adding liveness EndPoint (#35)

---
+++ src/main/java/com/tarento/commenthub/controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.tarento.commenthub.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/liveness")
+    public ResponseEntity<?> livenessCheck() throws Exception {
+        return new ResponseEntity<>("Status ok", HttpStatus.OK);
+    }
+}
